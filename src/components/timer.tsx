dd3864873400c4cf85f7bf238501fa221976e87e
Gabriel Souza <104937852+gabrielsouzas@users.noreply.github.com>
2024-10-15 16:47:15 -0300

    fix: correção tempo em horas trabalhadas

--- src/components/timer.tsx
+++ src/components/timer.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import { secondToTime } from '../utils/second-to-time';
+import { secondToMinutes } from '../utils/seconds-to-minutes';
 
 interface Props {
   mainTime: number;
 }
 
 export function Timer(props: Props): JSX.Element {
-  return <div className="timer">{secondToTime(props.mainTime)}</div>;
+  return <div className="timer">{secondToMinutes(props.mainTime)}</div>;
 }
